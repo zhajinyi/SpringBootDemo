fe4ed79eb4694b6b4b1d524f2912ed5ef953b375
Mirakyux <mirakyux@163.com>
2019-08-28 10:17:26 +0800

    init project

---
+++ searchfun/src/main/java/org/dream/searchfun/SearchfunApplication.java
@@ -0,0 +1,13 @@
+package org.dream.searchfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchfunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchfunApplication.class, args);
+    }
+
+}
